49d535875b1dafee7711f4a7449bb16d3d1b633a
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-03-08 10:49:09 +0000

    Update searchless.js

--- searchless.js
+++ searchless.js
@@ -3,14 +3,14 @@ console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 function setFromRequest()
 {
-  var request = urlParams.get("q")
-  if (request.length < 1)
+  var q = urlParams.get('q')
+  if (!urlParams.has('q'))
   {
     document.getElementById("mendel") = "Can't find a search request. Which is just as well, because nothing much would change if you did have one.";
   }
   else
   {
-    document.getElementById("mendel") = "Search request is " + request + ". Entry are denied.";
+    document.getElementById("mendel") = "Search request is " + q + ". Entry are denied.";
   }
 }
 setFromRequest();

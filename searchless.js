e04b98022247f1d187c59a9687b24256632246ed
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-03-08 10:14:48 +0000

    Create searchless.js

---
+++ searchless.js
@@ -0,0 +1,20 @@
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
+}
+function setFromRequest()
+{
+  if (request.length < 1)
+  {
+    document.getElementById('mendel') = "Can't find a search request. Which is just as well, because nothing much would change if you did have one.";
+  }
+  else
+  {
+    document.getElementById('mendel') = "Your search request is " + request + ". Entry are denied.";
+  }
+}
+var request = getUrlVars()["q"];
+setFromRequest();

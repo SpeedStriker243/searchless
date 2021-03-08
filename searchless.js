d513d354b28518ec5cbeabbb8ea97caec3443c49
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-03-08 10:34:19 +0000

    Update searchless.js

--- searchless.js
+++ searchless.js
@@ -9,11 +9,11 @@ function setFromRequest()
 {
   if (request.length < 1)
   {
-    document.getElementById('mendel') = "Can't find a search request. Which is just as well, because nothing much would change if you did have one.";
+    document.getElementById("mendel") = "Can't find a search request. Which is just as well, because nothing much would change if you did have one.";
   }
   else
   {
-    document.getElementById('mendel') = "Search request is " + request + ". Entry are denied.";
+    document.getElementById("mendel") = "Search request is " + request + ". Entry are denied.";
   }
 }
 var request = getUrlVars()["q"];
